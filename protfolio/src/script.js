689de53ea38344bbb8bf236e33d0643d48899e90
IGhos-P <dndb3599@gmail.com>
2022-04-21 13:41:18 +0900

    feat : three.jss 적용완료

--- protfolio/src/script.js
+++ protfolio/src/script.js
@@ -143,14 +143,42 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 
+document.addEventListener("mousemove", onDocumentMouesMove);
+
+let mouseX = 0;
+let mouseY = 0;
+
+let targetX = 0;
+let targetY = 0;
+
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+
+function onDocumentMouesMove(event) {
+  mouseX = (event.clientX - windowHalfX) / 2; // 이벤트 객체에서 현재 보이는 화면의 수평값 - 전체 화면의 수평값
+  mouseY = (event.clientY - windowHalfY) / 2; // 이벤트 객체에서 현재 보이는 화면의 수직값 - 전체 화면의 수직값
+}
+// 스크롤을 할대 위로 올라가는 느낌을 줘보자
+window.addEventListener("scroll", updataShpere);
+
+function updataShpere(event) {
+  sphere.position.y = window.scrollY * 0.001;
+}
 const clock = new THREE.Clock();
 
 const tick = () => {
+  targetX = mouseX * 0.001;
+  targetY = mouseY * 0.001;
+
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
   sphere.rotation.y = 0.5 * elapsedTime;
 
+  // 변경되는 값을 적용시키기
+  sphere.rotation.y += 0.5 * (targetX - sphere.rotation.y);
+  sphere.rotation.x += 0.5 * (targetY - sphere.rotation.x);
+  sphere.position.z += -0.05 * (targetY - sphere.rotation.x);
   // Update Orbital Controls
   // controls.update()
 

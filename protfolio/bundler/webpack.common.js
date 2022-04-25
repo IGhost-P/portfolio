13065ce3354ec8d3b912b3ede8470b1ded8cefa3
IGhos-P <dndb3599@gmail.com>
2022-04-25 13:34:41 +0900

    feat: img 넣기

--- protfolio/bundler/webpack.common.js
+++ protfolio/bundler/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
   resolve: {
     alias: {
       components: path.resolve(__dirname, "../src/components"),
-      utils: path.resolve(__dirname, "../src/utils/"),
+      utils: path.resolve(__dirname, "./client/utils/"),
     },
   },
   module: {
